02c66d74d163f1535878aa1684c74dc6a51ce6fc
MogiliManikanta <you@example.com>
2024-12-14 10:06:00 +0530

    setup completed

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-heloo;
+const express = require("express");
+const dotEnv = require("dotenv");
+const cors = require("cors");
+dotEnv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server is running on port ${process.env.PORT}`)
+);
